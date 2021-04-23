2bb563965fe541cc8598724756e25ce37637eaed
SergioCabrera <sergiodcr0258@gmail.com>
2021-04-23 15:14:14 -0600

    Comenzando con el modelo de las tareas

---
+++ Lab5/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab5.Models;
+using System.Reflection;
+using System.IO;
+
+namespace Lab5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab5/Models/TareaStruct.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab5.Models
+{
+    public class TareaStruct : IComparable
+    {
+        //Declaracion de variables
+        [Display(Name = "Titulo")]
+        public string Titulo { get; set; }
+        [Display(Name = "Descripcion")]
+        public string Descripcion { get; set; }
+        [Display(Name = "Proyecto")]
+        public string Proyecto { get; set; }
+        public int Prioridad { get; set; }
+        [Display(Name = "FechaEntrega")]
+        public string FechaEntrega { get; set; }
+
+
+        //Comparaciones
+        public int CompareTo(object obj)
+        {
+            return this.Titulo.CompareTo(((TareaStruct)obj).Titulo);
+        }
+        public static Comparison<TareaStruct> CompararPorTitulo = delegate (TareaStruct tarea1, TareaStruct tarea2)
+        {
+            return tarea1.Titulo.CompareTo(tarea2.Titulo);
+        };
+        public static Comparison<TareaStruct> CompararPorDescripcion = delegate (TareaStruct tarea1, TareaStruct tarea2)
+        {
+            return tarea1.Descripcion.CompareTo(tarea2.Descripcion);
+        };
+        public static Comparison<TareaStruct> CompararPorProyecto = delegate (TareaStruct tarea1, TareaStruct tarea2)
+        {
+            return tarea1.Proyecto.CompareTo(tarea2.Proyecto);
+        };
+        public static Comparison<TareaStruct> CompararPorPrioridad = delegate (TareaStruct tarea1, TareaStruct tarea2)
+        {
+            return tarea1.Prioridad.CompareTo(tarea2.Prioridad);
+        };
+        public static Comparison<TareaStruct> CompararPorFechaEntrega = delegate (TareaStruct tarea1, TareaStruct tarea2)
+        {
+            return tarea1.Titulo.CompareTo(tarea2.FechaEntrega);
+        };
+    }
+}
